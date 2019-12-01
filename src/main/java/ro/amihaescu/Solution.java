7f55049928b0b812f05614033b16e0e4460ca2e7
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2019-12-01 20:47:38 +0200

    - refactored and solved problem

--- src/main/java/ro/amihaescu/Solution.java
+++ src/main/java/ro/amihaescu/Solution.java
@@ -3,11 +3,52 @@ package ro.amihaescu;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.binarySearch;
+import static java.util.stream.IntStream.range;
+
 public class Solution {
 
-    public int pickingNumbers(List<Integer> list) {
+    String condenseArray(int[] array) {
+        boolean isSequence = false;
+        StringBuilder sb = new StringBuilder();
+        for (int index = 0; index < array.length; index++) {
+            int diff = diff(array, index);
+            if (diff != 1 && !isSequence) {
+                sb.append(array[index]);
+                if (index != array.length - 1) sb.append(",");
+            } else if (diff == 1 && !isSequence) {
+                isSequence = true;
+                sb.append(array[index]).append("-");
+            } else if (diff != 1) {
+                sb.append(array[index]);
+                if (index != array.length - 1) sb.append(",");
+                isSequence = false;
+            }
+        }
+        return sb.toString();
+    }
+
+    private static int diff(int[] array, int position) {
+        if (position == array.length - 1)
+            return -1;
+        return array[position + 1] - array[position];
+    }
+
+    int pickingNumbers(List<Integer> list) {
         return list.stream()
                 .mapToInt(integer -> Collections.frequency(list, integer) + Collections.frequency(list, integer + 1))
                 .max().orElse(0);
+    }
+
+    int[] climbingLeaderBoard(int[] scores, int[] alice) {
+        int[] positions = new int[alice.length];
+        int[] array = range(0, scores.length).map(i -> scores[scores.length - i - 1]).distinct().toArray();
+        int index = 0;
+        for (int scoreIndex = 0; scoreIndex < alice.length;  scoreIndex++) {
+            index = binarySearch(array, Math.max(index, 0), array.length, alice[scoreIndex]);
+            if (index < 0) index = -index - 2;
+            positions[scoreIndex] = array.length - index;
+        }
+        return positions;
     }
 }
--- src/test/java/ro/amihaescu/SolutionTest.java
+++ src/test/java/ro/amihaescu/SolutionTest.java
@@ -1,7 +1,6 @@
 package ro.amihaescu;
 
 import org.junit.jupiter.api.Test;
-import ro.amihaescu.Solution;
 
 import java.util.Arrays;
 
@@ -12,12 +11,71 @@ public class SolutionTest {
     private Solution solution = new Solution();
 
     @Test
-    void test1() {
+    void PickingNumbers_Sample1() {
         assertEquals(3, solution.pickingNumbers(Arrays.asList(4,6,5,3,3,1)));
     }
 
     @Test
-    void test2() {
+    void PickingNumbers_Sample2() {
         assertEquals(5, solution.pickingNumbers(Arrays.asList(1,2,2,3,1,2)));
+    }
+
+    @Test
+    void ClimbingLeaderBoard_Sample1() {
+        int[] result = solution.climbingLeaderBoard(new int[]{100, 100, 50, 40, 40, 20, 10}, new int[]{5 ,25, 50, 120});
+        assertEquals(6, result[0]);
+        assertEquals(4, result[1]);
+        assertEquals(2, result[2]);
+        assertEquals(1, result[3]);
+    }
+
+    @Test
+    void ClimbingLeaderBoard_Sample2() {
+        int[] result = solution.climbingLeaderBoard(new int[]{100, 90, 90, 80, 75, 60}, new int[]{50, 65, 77, 90, 102});
+        assertEquals(6, result[0]);
+        assertEquals(5, result[1]);
+        assertEquals(4, result[2]);
+        assertEquals(2, result[3]);
+        assertEquals(1, result[4]);
+    }
+
+    @Test
+    void testCondensedIntervals_TwoSequences(){
+        int[] array = {1,2,4,6,7,8,9,11};
+        String expected = "1-2,4,6-9,11";
+
+        String actual = solution.condenseArray(array);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCondensedIntervals_OneSequence(){
+        int[] array = {1,6,7,8,9};
+        String expected = "1,6-9";
+
+        String actual = solution.condenseArray(array);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCondensedIntervals_OnlyOneSequence(){
+        int[] array = {6,7,8,9};
+        String expected = "6-9";
+
+        String actual = solution.condenseArray(array);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCondensedIntervals_NoSequence(){
+        int[] array = {1,3,5,7};
+        String expected = "1,3,5,7";
+
+        String actual = solution.condenseArray(array);
+
+        assertEquals(expected, actual);
     }
 }
