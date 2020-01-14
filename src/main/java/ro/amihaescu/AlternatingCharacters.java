ce56dc0a9a07fab011c83a5c2843be6fd11710c3
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2020-01-14 08:58:45 +0200

    -alternating characters

---
+++ src/main/java/ro/amihaescu/AlternatingCharacters.java
@@ -0,0 +1,23 @@
+package ro.amihaescu;
+
+public class AlternatingCharacters {
+
+    static int alternatingCharacters(String s) {
+        int index = 0;
+        int noDeletions = 0;
+        Character character = s.charAt(index);
+        while (++index < s.length()) {
+            Character itterator = s.charAt(index);
+            if (itterator == character) {
+                noDeletions++;
+            } else {
+                character = itterator;
+            }
+        }
+        return noDeletions;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(alternatingCharacters("AAABBB"));
+    }
+}
