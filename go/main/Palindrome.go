b65ec94272ea4142076a039600e174d354dd4f8d
Andrei Mihaescu <amihaesc@cisco.com>
2023-10-05 14:59:03 +0300

    palindrom

---
+++ go/main/Palindrome.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	stringX := fmt.Sprintf("%d", x)
+	length := len(stringX)
+	if length == 0 || length == 1 {
+		return true
+	}
+	for i := 0; i < length/2; i++ {
+		if stringX[i] != stringX[length-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	print(isPalindrome(10))
+}
